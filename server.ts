4f98103d1a65b9c2f5237103189d6f305c7a0658
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2023-10-25 14:50:44 -0700

    Remove Express

--- server.ts
+++ server.ts
@@ -1,16 +1,9 @@
 import http from "http";
-import express from "express";
-import path from "path";
 
-const app = express();
-app.use(express.static(path.resolve(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.header('Content-Type', 'text/html');
+http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
   res.write("hello");
   res.end("end");
-})
-
-http.createServer(app).listen(3000, () => {
+}).listen(3000, () => {
   console.log('http://localhost:3000');
 });
